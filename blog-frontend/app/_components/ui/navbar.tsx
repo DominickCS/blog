88beed4f6267f0a27aaa2e9a5d0edd91186a6990
Dominick Smith <dominick.smith938@gmail.com>
2026-01-15 22:36:44 -0600

    Building blog slug

--- blog-frontend/app/_components/ui/navbar.tsx
+++ blog-frontend/app/_components/ui/navbar.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function NavigationBar() {
   return (
-    <div>
-      <h1 className="text-4xl font-light text-red-400 text-center my-4">Developing with DominickCS</h1>
+    <div className="mx-auto text-center">
+      <Link href={'/'} className="text-4xl font-light text-red-400 my-4">Developing with DominickCS</Link>
     </div>
   )
 }
--- blog-frontend/app/post/[post-slug]/page.tsx
+++ blog-frontend/app/post/[post-slug]/page.tsx
@@ -2,6 +2,9 @@
 import NavigationBar from "@/app/_components/ui/navbar"
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react"
+import HeartSVG from "@/public/heart.svg"
+import SaveSVG from "@/public/save.svg"
+import Image from "next/image"
 
 
 export default function BlogPost() {
@@ -35,11 +38,34 @@ export default function BlogPost() {
   console.log(blogPost)
 
   if (!loading && blogPost) {
+    const date = new Date(blogPost.blogPublishDate).toLocaleDateString() + " at " + new Date(blogPost.blogPublishDate).toLocaleTimeString()
     return (
-      <>
+      <div>
         <NavigationBar />
-        Hello, World!
-      </>
+        <div className="bg-white rounded-md max-w-xl mx-auto p-8 my-8 border-8 border-black/10 shadow-lg shadow-black/60 dark:shadow-white/60">
+          <h1 className="text-center text-3xl font-medium mb-4">{blogPost.blogTitle}</h1>
+          <hr className="my-6 max-w-xs mx-auto" />
+          <div className="flex text-sm font-light items-center mt-2 justify-evenly">
+            {blogPost.blogTags.map((tag, id) => {
+              return <p className="" key={id}>{tag}</p>
+            })}
+            <p className="">{date}</p>
+          </div>
+          <div className="my-10">
+            <p className="">{blogPost.blogBody}</p>
+            <hr className="my-6 max-w-xs mx-auto" />
+          </div>
+          <div>
+            <h2 className="text-xl font-medium">Comments</h2>
+          </div>
+          <div className="mt-8 flex justify-end text-center">
+            <p className="mx-4"><Image src={HeartSVG} width={20}></Image> {blogPost.blogLikeCount}</p>
+            <p className="mx-4"><Image src={SaveSVG} width={20}></Image>{blogPost.blogSaveCount}</p>
+          </div>
+          <div>
+          </div>
+        </div>
+      </div>
     )
   } else {
     return (
